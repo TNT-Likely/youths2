2f32ce86c6d4a8ac9c95b21d09334d2d590c2d28
likely <532837139@qq.com>
2016-10-16 02:41:12 +0800

    deploy script

---
+++ deploy.js
@@ -0,0 +1,21 @@
+import serveStatic from 'serve-static'
+import http from 'http'
+import config from './config'
+import finalhandler from 'finalhandler'
+
+let serve = serveStatic(config.root.dist, { extensions: ['html', 'htm'] })
+
+// Create server 
+let server = http.createServer((req, res) => {
+  serve(req, res, finalhandler(req, res))
+})
+
+//clientErr event
+server.on('clientError', (err, socket) => {
+  socket.end('HTTP/1.1 400 Bad Request\r\n\r\n');
+})
+
+// Listen 
+server.listen(config.port.prod, () => {
+  console.log(`listen at ${config.port.prod} success!`)
+})
