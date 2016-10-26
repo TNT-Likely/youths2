5c37f92990d7bf005c68f248d79e2b98d948263d
likely <532837139@qq.com>
2016-10-26 18:49:17 +0800

    not use bootstrap

--- app/js/modules/index.js
+++ app/js/modules/index.js
@@ -6,10 +6,12 @@ require('../../scss/pages/index.scss')
 new vue({
   el: '#app',
   data: {
+    headerItems: [],
     items: []
   },
   created: function() {
     fetch('/nba/stats/leagueleaders?LeagueID=00&PerMode=PerGame&Scope=S&Season=2016-17&SeasonType=Pre+Season&StatCategory=PTS').then(r => {
+      this.headerItems = r.data.resultSet.headers
       this.items = r.data.resultSet.rowSet.slice(0, 11)
     })
   }
