55b17b52339f29acdb62530399414af6120ba4ed
likely <532837139@qq.com>
2016-11-05 16:17:21 +0800

    toast module

--- app/js/components/toast.js
+++ app/js/components/toast.js
@@ -1,13 +1,32 @@
-import toast from 'toastr'
+class toast {
+  constructor(msg) {
+    require('../../scss/components/_toast.scss')
+    if (typeof msg != 'string') return;
+    this.msg = msg
+    this.init()
+  }
 
-let info = (msg) => {
-  toast(msg)
+  init() {
+    let html = '<div class="toast">'
+    html += this.msg
+    html += '<div>'
+    this.html = html
+    this.el = $('.toast')
+    this.show()
+  }
+
+  show() {
+    if (this.el.length > 0) this.el.remove();
+    $('body').append(this.html)
+    let self = this
+    setTimeout(() => {
+      self.hide()
+    }, 1800)
+  }
+
+  hide() {
+    $('.toast').hide()
+  }
 }
 
-let errorFun = (msg) => {
-  toast('错误', msg, 'error')
-}
-
-export default info
-
-export let error = errorFun
+export default toast
--- app/js/modules/user/login.js
+++ app/js/modules/user/login.js
@@ -1,2 +1,5 @@
 require('../../../scss/pages/user/login.scss')
 require('captcha')
+import toast from 'toast'
+
+toast('test')
