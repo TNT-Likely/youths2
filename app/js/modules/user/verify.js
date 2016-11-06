3b75dd1d2ade008a35c467cf5f89dc6caca8017c
likely <532837139@qq.com>
2016-11-06 15:50:21 +0800

    激活模块

---
+++ app/js/modules/user/verify.js
@@ -0,0 +1,10 @@
+require('../../../scss/pages/user/verify.scss')
+require('header')
+import { toast, fetch, validate, query, cookies } from 'tool'
+
+fetch('/rest/user/verify', 'POST', { uid: query('uid'), token: query('token') }).then(r => {
+  $('.success').show()
+}).catch(e => {
+  // $('.failed p').text(e.msg)
+  $('.failed').show()
+})
