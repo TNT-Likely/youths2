bd345521b7940f591d51bdaaaf8d50fb4f32a8d0
likely <532837139@qq.com>
2016-11-19 00:02:35 +0800

    style

--- app/js/bussiness/user.js
+++ app/js/bussiness/user.js
@@ -1,6 +1,9 @@
 import { fetch, bind } from 'tool'
 import { header } from 'bussiness'
 
+//个人中心导航栏激活状态
+$(`[href="${location.pathname}"]`).parent('li').addClass('active')
+
 export default () => {
   return new Promise((resolve, reject) => {
     header().then(r => {
